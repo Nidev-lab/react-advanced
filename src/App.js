8a16dfaa83b52b318ca15a55f83658743b8ccece
Nicolas Gonzalez <nicogonzalez2008@gmail.com>
2022-07-21 21:33:20 -0300

    clase-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Card } from './components/Card';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const [idPersonaje, setIdPersonaje] = useState(1);
 
-  const getCharacter = async() => {
+  const getCharacter = async () => {
     setIsLoading(true);
 
     const { data } = await axios('https://rickandmortyapi.com/api/character');
@@ -16,15 +18,34 @@ const App = () => {
   };
 
   useEffect(() => {
-    getCharacter()
-  }, [])
-  
+    getCharacter();
+  }, []);
+
+  const getIdPersonaje = (id) => {
+    setIdPersonaje(id);
+  };
+
+  const getCharacterById = async () => {
+    const { data } = await axios(`https://rickandmortyapi.com/api/character/${idPersonaje}`);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getCharacterById();
+  }, [idPersonaje]);
+
   return (
     <div className="App">
       { isLoading && <p>Loading...</p> }
-      { characters?.map(character => <p key={character.id}>{character.type}</p>) }
+      { characters?.map(character => (
+        <Card
+          character={character}
+          key={character.id}
+          getIdPersonaje={getIdPersonaje}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
