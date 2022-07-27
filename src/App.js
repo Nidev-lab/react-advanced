f9fb07d1d8a49f7ee6af25c2d3b159b9ca2bbb48
dante-sarmiento <dante.sarmientof5@gmail.com>
2022-07-27 18:48:25 -0300

    feat(eslint): Eslint added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import axios from 'axios';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [characters, setCharacters] = useState([]);
 
-  const getCharacter = async() => {
+  const getCharacter = async () => {
     setIsLoading(true);
-
     const { data } = await axios('https://rickandmortyapi.com/api/character');
     setCharacters(data.results);
 
@@ -16,15 +15,16 @@ const App = () => {
   };
 
   useEffect(() => {
-    getCharacter()
-  }, [])
-  
+    getCharacter();
+  }, []);
+
   return (
     <div className="App">
       { isLoading && <p>Loading...</p> }
       { characters?.map(character => <p key={character.id}>{character.type}</p>) }
+      <p>otra branch de prueba je</p>
     </div>
   );
-}
+};
 
 export default App;
