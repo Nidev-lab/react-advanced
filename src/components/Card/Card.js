7554cec0a32473f0ee7c599e46d85098967c6341
nicode <nicogonzalez2008@gmail.com>
2022-08-09 19:34:26 -0300

    feature/useContext added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import classNames from 'classnames';
+import { useThemeContext } from '../../providers/ThemeProvider';
 
 const Card = ({ character, getIdPersonaje }) => {
+  const theme = useThemeContext();
+
   const handleClick = () => {
     getIdPersonaje(character.id);
   };
@@ -8,10 +12,13 @@ const Card = ({ character, getIdPersonaje }) => {
   return (
     <div className="card">
       <img src={character.image} className="card-img-top" alt="..." />
-      <div className="card-body">
+      <div className={classNames('card-body', {
+        'bg-light': theme === 'light',
+        'bg-dark text-white': theme === 'dark',
+      })}>
           <h5 className="card-title">{character.name}</h5>
           <p className="card-text">{character.species}</p>
-          <button className="btn btn-primary" onClick={handleClick}>Go somewhere</button>
+          <button className={`btn ${theme === 'light' ? 'btn-primary' : 'btn-success'}`} onClick={handleClick}>Go somewhere</button>
       </div>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import classNames from 'classnames';
+import React from 'react';
+import { useThemeContext, useThemeToggleContext } from '../../providers/ThemeProvider';
+
+const Header = () => {
+  const setTheme = useThemeToggleContext();
+  const theme = useThemeContext();
+
+  const handleChange = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+
+  return (
+    <nav className={classNames('navbar navbar-light', {
+      'bg-light': theme === 'light',
+      'bg-dark': theme === 'dark',
+    })}>
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Navbar</span>
+      </div>
+      <button className='btn-primary' onClick={() => setTheme('dark')}>Dark</button>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={handleChange}/>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
