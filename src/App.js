d7ebee2a1f2f0e7cc11e77f1ae634a9657f2f4e0
nicode <nicogonzalez2008@gmail.com>
2022-09-06 19:38:17 -0300

    conflicts fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { Card } from './components/Card';
-import { Header } from './components/Header';
 import useFetch from './hooks/CustomFetch/useFetch';
-import { ThemeProvider } from './providers/ThemeProvider';
 
 const App = () => {
   const [idPersonaje, setIdPersonaje] = useState(1);
@@ -24,21 +22,6 @@ const App = () => {
   }, [idPersonaje]);
 
   return (
-<<<<<<< HEAD
-    <ThemeProvider>
-      <div className="App">
-        <Header />
-        { dataPersonajes?.loading && <p>Loading...</p> }
-        { dataPersonajes?.data?.results.map(character => (
-          <Card
-            character={character}
-            key={character.id}
-            getIdPersonaje={getIdPersonaje}
-          />
-        ))}
-      </div>
-    </ThemeProvider>
-=======
     <div className="App">
       { dataPersonajes?.loading && <p>Loading...</p> }
       { dataPersonajes?.data?.results.map(character => (
@@ -49,7 +32,6 @@ const App = () => {
         />
       ))}
     </div>
->>>>>>> parent of 4be37c5 (ej. clase 8)
   );
 };
 
